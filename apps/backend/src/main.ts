bf9ed56215d55b9551979be30e3c1691d4f562b7
JHacin <jan.hacin@gmail.com>
2022-08-13 20:06:03 +0200

    install payload

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,18 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import * as express from 'express';
+import express from 'express';
+import payload from 'payload'
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to backend!' });
-});
+payload.init({
+  secret: 'SECRET_KEY',
+  mongoURL: 'mongodb://127.0.0.1:27017/payload',
+  express: app,
+})
 
 const port = process.env.port || 3333;
+
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Ready on http://localhost:${port}/admin`);
 });
+
 server.on('error', console.error);
